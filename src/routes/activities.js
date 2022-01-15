d12830817c1f630ae1fd0d20577df5da5d46ee5c
Tony <antoniotd87@gmail.com>
2022-01-14 21:24:25 -0600

    Correccion de las fechas

--- src/routes/activities.js
+++ src/routes/activities.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const moment = require('moment');;
 
 const Note = require('../models/Activity');
 const { isAuthenticated } = require('../helpers/salida');
@@ -9,7 +10,7 @@ router.get('/activities/add', isAuthenticated, (req, res) => {
     res.render('activity/new-activity');
 });
 
-router.post('/activities/new-activity', isAuthenticated, async(req, res) => {
+router.post('/activities/new-activity', isAuthenticated, async (req, res) => {
     const { title, description, calendar_start, calendar_finish } = req.body;
     const errors = [];
     if (!title) {
@@ -27,30 +28,41 @@ router.post('/activities/new-activity', isAuthenticated, async(req, res) => {
     } else {
         const newNote = new Note({ title, description, calendar_start, calendar_finish });
         newNote.user = req.user.id;
+        newNote.calendar_start = new Date(newNote.calendar_start).getTime() + 21600000;
+        newNote.calendar_finish = new Date(newNote.calendar_finish).getTime() + 21600000;
         await newNote.save();
         req.flash('success_msg', 'Actividad Agregada');
         res.redirect('/activities');
     }
 });
 // mostrar todas las actividades
-router.get('/activities', isAuthenticated, async(req, res) => {
+router.get('/activities', isAuthenticated, async (req, res) => {
     const notes = await Note.find({ user: req.user.id }).sort({ date: 'desc' });
     res.render('activity/activities', { notes });
 });
 
-router.get('/activities/edit/:id', isAuthenticated, async(req, res) => {
-    const note = await Note.findById(req.params.id);
-    res.render('activity/edit-activity', { note });
+router.get('/activities/edit/:id', isAuthenticated, async (req, res) => {
+    let note = await Note.findById(req.params.id);
+    let calendar_start = new Date(note.calendar_start).toISOString().substring(0, 10)
+    let calendar_finish = new Date(note.calendar_finish).toISOString().substring(0, 10)
+    res.render('activity/edit-activity', { note, calendar_finish, calendar_start });
 });
 
-router.put('/activities/edit-activity/:id', isAuthenticated, async(req, res) => {
-    const { title, description, calendar_start, calendar_finish } = req.body;
-    await Note.findByIdAndUpdate(req.params.id, { title, description, calendar_start, calendar_finish });
+router.put('/activities/edit-activity/:id', isAuthenticated, async (req, res) => {
+    let { title, description, calendar_start, calendar_finish } = req.body;
+    calendar_start = new Date(calendar_start).getTime() + 21600000;
+    calendar_finish = new Date(calendar_finish).getTime() + 21600000;
+    await Note.findByIdAndUpdate(req.params.id, {
+        title,
+        description,
+        calendar_start,
+        calendar_finish
+    });
     req.flash('success_msg', 'Actividad Actualizada!!!');
     res.redirect('/activities')
 });
 
-router.delete('/activities/delete/:id', isAuthenticated, async(req, res) => {
+router.delete('/activities/delete/:id', isAuthenticated, async (req, res) => {
     await Note.findByIdAndDelete(req.params.id);
     req.flash('success_msg', 'Actividad Eliminada!!!');
     res.redirect('/activities');
